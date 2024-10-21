c014a65cf9c268a65515bff6a2b8d2b065cff18e
VGBrandon <villegas.galvan.brandon@gmail.com>
2024-10-21 06:46:00 -0500

    add execution of install-official-packages.sh

--- .config/install/install.sh
+++ .config/install/install.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+# Colores para la salida
+GREEN="\e[32m"
+RED="\e[31m"
+NC="\e[0m" # No color
+
+# Función para verificar si un archivo existe
+check_file_exists() {
+    local file="$1"
+    if [[ -f "$file" ]]; then
+        echo -e "${GREEN}Llamando a $file...${NC}"
+        bash "$file"
+    else
+        echo -e "${RED}Error: $file no encontrado.${NC}"
+        exit 1
+    fi
+}
+
+# Mensaje de inicio
+echo -e "${GREEN}Iniciando la instalación de dotfiles...${NC}"
+
+# Verificar y ejecutar el script de instalación de paquetes oficiales
+echo -e "${GREEN}Instalando Paquetes Oficiales de Archlinux...${NC}"
+check_file_exists "./install-official-packages.sh"
+echo -e "${GREEN}Se termino de instalar los paquetes oficiales de Archlinux...${NC}"
+
+
+echo -e "${GREEN}Instalación de dotfiles completada.${NC}"
