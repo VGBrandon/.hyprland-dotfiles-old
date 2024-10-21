3b2b1c8ab79138eaac606dfb0ce4a4227cca127a
VGBrandon <105024943+VGBrandon@users.noreply.github.com>
2024-10-21 12:33:33 -0500

    add adittional config

--- .config/install/install.sh
+++ .config/install/install.sh
@@ -45,4 +45,36 @@ check_file_exists "./install-aur-packages.sh"
 echo -e "${GREEN}Cambiando la shell predeterminada a Zsh...${NC}"
 chsh -s $(which zsh)
 
+# Verificar y ejecutar stow para crear los symlinks
+echo -e "${GREEN}Creando symlinks de mis dotfiles...${NC}"
+check_file_exists "./rename-exist-files-for-execute-stow.sh"
+
+echo -e "${BLUE}Instalando gtk de gruvbox-material...${NC}"
+git clone https://github.com/TheGreatMcPain/gruvbox-material-gtk.git
+cp -r gruvbox-material-gtk/themes/* ~/.themes
+cp -r gruvbox-material-gtk/icons/* ~/.icons
+sudo rm -rf gruvbox-material-gtk
+nwg-look -a
+
+# Instalar ohmyzsh y plugins
+echo -e "${BLUE}Instalando Oh My Zsh y plugins...${NC}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#Plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# Editando el .zshrc para usar los plugins
+echo -e "${CYAN}Configurando .zshrc...${NC}"
+# Modificar el archivo .zshrc para actualizar la línea de plugins
+sed -i '/^plugins=(git)/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
+# Mensaje de éxito
+echo -e "${GREEN}.zshrc configurado correctamente.${NC}"
+# Esto talvez se cambie en el futuro y sea un archivo de mis dotfiles
+
+# Agregar configuracion adicional para Git
+echo -e "${BLUE}Agregando configuracion adicional de git para las llaves...${NC}"
+git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
+
+# Verificar y ejecutar script para reiniciar
+check_file_exists "./reboot.sh"
+
 echo -e "${GREEN}Instalación de dotfiles completada.${NC}"
