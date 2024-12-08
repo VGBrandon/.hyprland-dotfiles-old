bf36cd2adb3dcf8d580086794fadeb3700938184
VGBrandon <villegas.galvan.brandon@gmail.com>
2024-12-08 10:28:57 -0500

    update

--- .config/nvim/lua/plugins/comment-nvim.lua
+++ .config/nvim/lua/plugins/comment-nvim.lua
@@ -1,5 +1,6 @@
 return {
 	-- para comentar una linea debes presionar gcc
+	-- para comentar en bloque usa gb seleccionando el texto o gbc sin seleccionar
 	"numToStr/Comment.nvim",
 	opts = {
 		-- add any options here
