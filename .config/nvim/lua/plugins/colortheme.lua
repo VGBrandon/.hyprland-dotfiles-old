60a0e85c0c763f4c67f35b2c67caab9f933074bc
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-14 18:38:24 -0500

    change new theme

--- .config/nvim/lua/plugins/colortheme.lua
+++ .config/nvim/lua/plugins/colortheme.lua
@@ -1,4 +1,4 @@
-  return{
+--[[ return{
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
@@ -8,4 +8,17 @@
       vim.g.gruvbox_material_enable_italic = true
       vim.cmd.colorscheme('gruvbox-material')
     end
-  }
+  } ]]
+
+return {
+	"RRethy/base16-nvim",
+	lazy = false, -- No es lazy-loaded
+	priority = 1000, -- Prioridad alta
+	config = function()
+		-- Aquí no es necesario, pero si quieres aplicar alguna configuración, puedes hacerlo.
+		-- Vim no necesita configuraciones especiales para cargar los esquemas de Base16.
+
+		-- Aplicamos el esquema de colores Base16 Ashes
+		vim.cmd.colorscheme("base16-ashes")
+	end,
+}
