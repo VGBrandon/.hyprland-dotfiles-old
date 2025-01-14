e05f5847e9f478ba59da29c4c80f97281869ad1b
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-14 18:56:08 -0500

    add option for remove ~ for " "

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,9 +1,9 @@
 vim.wo.number = true
 vim.o.relativenumber = true
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.wrap = false
 vim.o.linebreak = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.autoindent = true
 vim.o.ignorecase = true
 vim.o.shiftwidth = 2
@@ -18,14 +18,16 @@ vim.o.splitright = true
 vim.o.hlsearch = false
 vim.o.showmode = false
 vim.opt.termguicolors = true
-vim.o.whichwrap = 'bs<>[]hl'
-vim.o.numberwidth = 4
+vim.o.whichwrap = "bs<>[]hl"
+vim.o.numberwidth = 3
 vim.o.swapfile = false
 vim.o.smartindent = true
 vim.o.showtabline = 2
-vim.o.backspace = 'indent,eol,start'
+vim.o.backspace = "indent,eol,start"
 vim.o.pumheight = 10
 vim.o.conceallevel = 0
-vim.wo.signcolumn = 'yes'
-vim.o.fileencoding = 'utf-8'
+vim.wo.signcolumn = "yes"
+vim.o.fileencoding = "utf-8"
 vim.o.cmdheight = 1
+-- Evitar que se muestren los ~ en Neovim
+vim.opt.fillchars = { eob = " " }
