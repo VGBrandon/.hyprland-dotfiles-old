8b2bea133cf684b6398a25dfd19f972e2d84cc26
VGBrandon <villegas.galvan.brandon@gmail.com>
2025-01-14 21:48:54 -0500

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,29 +11,6 @@ config = {
   color_scheme = "Ashes (base16)",
   font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
   font_size = 10.6,
-  --[[ background = {
-    {
-      --Imagen
-      source = {
-        File = "/home/vgbrandon/Pictures/wallpaper2.jpeg",
-      },
-      hsb = {
-        hue = 1.0,
-        saturation = 1.02,
-        brightness = 0.25,
-      },
-      width = "100%",
-      height = "100%",
-    },
-    {
-      source = {
-        Color = "#1F2020",
-      },
-      width = "100%",
-      height = "100%",
-      opacity = 0.55,
-    },
-  }, ]]
   window_padding = {
     left = 20,
     right = 20,
